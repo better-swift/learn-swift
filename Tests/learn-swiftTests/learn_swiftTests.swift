89b4ccb6961a91e66ca919e18de572c88220d70a
Henry.Huang <hhglory@outlook.com>
2024-01-08 10:39:36 +0800

    chore: update

---
+++ Tests/learn-swiftTests/learn_swiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import learn_swift
+
+final class learn_swiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(learn_swift().text, "Hello, World!")
+    }
+}
